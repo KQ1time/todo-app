aae6b6e743263a77682f23f61e5e8291824738b6
Alexander <kq1time.me@mail.ru>
2026-01-01 17:48:21 +0300

    Add module architecture

--- src/todo_app/main.py
+++ src/todo_app/main.py
@@ -1,47 +1,8 @@
 # version 0.1.1
 
-import json
+from task_manager import TaskManager
 
 # Program for creating tasks
-
-class TaskManager:
-
-    def __init__(self, filename):
-        self.filename = filename
-
-
-    def create(self, tasks, task_id, task):
-        with open(self.filename, "w") as f:
-            tasks[task_id] = task
-            json.dump(tasks, f, indent=4)
-
-    
-    def delete(self, tasks, task_id):
-        del tasks[task_id]
-
-        with open(self.filename, "w") as file:
-            json.dump(tasks, file)
-    
-    def create_id(self, tasks):
-        count = 1
-
-        while str(count) in tasks:
-            count += 1
-
-        return str(count)
-
-    
-    def tasks_read(self):
-        try:
-            with open(self.filename, "r") as file:
-                tasks = json.load(file)
-        
-        except (json.JSONDecodeError, FileNotFoundError):
-            with open(self.filename, "w") as file:
-                tasks = {}
-                json.dump(tasks, file)
-
-        return tasks
 
 
 def main():
